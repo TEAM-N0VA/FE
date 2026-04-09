137ab595a2e5452f7085caaf99131af7d3192b7d
yerinkang <yerinkang@ewha.ac.kr>
2026-04-09 15:00:50 +0900

    feat: 로고 이미지 추가 및 색상 정의

---
+++ constants/Colors.ts
@@ -0,0 +1,12 @@
+export const Colors = {
+  primary: '#FDE5F2',   // 배경색 (연한 핑크)
+  secondary: '#926897', // 로고 및 포인트 (보라)
+  white: '#F8F7F7',
+  text: '#2E292B',      // 가독성을 위한 짙은 회색
+};
+
+export const IconSizes = {
+  onboarding: 200,      // 온보딩 메인 일러스트
+  tabBar: 24,           // 하단 메뉴바 아이콘
+  touchArea: 44,        // 실제 터치 가능한 프레임 사이즈
+};
